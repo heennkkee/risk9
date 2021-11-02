c6cbd5fec6538486e2ae051fa9c6273c95a25bc1
Henrik <henrik.aronsson.94@gmail.com>
2021-11-02 23:04:11 +0100

    fun fun fun

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -17,38 +17,24 @@ function App() {
     const [assets, setAssets] = useState<r9.Asset[]>([]);
 
     useEffect(() => {
-        const callAsync = async () => {
+        const loadAssets = async () => {
             await api.assetGet().then((assets) => {
                 setAssets(assets);
             });
         }
-        callAsync();
+        loadAssets();
     }, []);
-
-    const removeAsset = async (id: number) => {
-        await api.assetIdDelete(id).then(() => {
-            setAssets([...assets.filter(asset => asset.assetId !== id)]);
-        });
-    }
-
-    const addAsset = async (inp: r9.AssetBinding) => {
-        await api.assetPost(inp).then(asset => {
-            setAssets([...assets, asset]);
-        }).catch(err => {
-            console.log("ERR", err);
-        });
-    }
     return (
         <div className="App">
             <table>
                 <tbody>
                     {assets.map(asset =>
-                        <tr>
+                        <tr key={asset.assetId}>
                             <td>{asset.assetId}</td>
                             <td>{asset.description}</td>
                             <td>{asset.description2}</td>
                             <td>{asset.numberThing}</td>
-                            <td><button onClick={() => removeAsset(asset.assetId)}>Remove</button></td>
+                            <td><button onClick={async () => await api.assetIdDelete(asset.assetId).then(() => setAssets([...assets.filter(x => x.assetId !== asset.assetId)]))}>Remove</button></td>
                         </tr>)}
                 </tbody>
             </table>
@@ -57,12 +43,12 @@ function App() {
                 Descr: <input type="text" ref={descrRef} /><br />
                 Descr2: <input type="text" ref={descr2Ref} /><br />
                 NumberThing: <input type="number" ref={numberThingRef} min={0} /><br />
-                <button onClick={() => {
-                    addAsset({
+                <button onClick={async () => {
+                    api.assetPost({
                         description: descrRef.current?.value || "",
                         description2: descr2Ref.current?.value,
                         numberThing: parseInt(numberThingRef?.current?.value || '0'),
-                    });
+                    }).then(newAsset => setAssets([...assets, newAsset]));
                 }}>Add</button>
             </div>
         </div>
