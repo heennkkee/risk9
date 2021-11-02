76be59a2793bb4325bce7bc69277dbeb9b2e8fd9
Henrik <henrik.aronsson.94@gmail.com>
2021-11-02 21:27:39 +0100

    wip fe

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+import * as AA from '@risk9/AssetApi';
+
 function App() {
+    const configuration = AA.createConfiguration({
+        baseServer: new AA.ServerConfiguration("http://localhost:5000", {})
+    });
+
+    const apiInstance = new AA.AssetApi(configuration);
+
+    useEffect(() => {
+        const callAsync = async () => {
+            await apiInstance.assetGet().then((assets) => {
+                assets.map(asset => {
+                    console.log(asset.assetId)
+                }
+            })
+        }
+        callAsync();
+    }, []);
   return (
     <div className="App">
       <header className="App-header">
